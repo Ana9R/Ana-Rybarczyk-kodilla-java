424c83d75652f6c71b3b0dd53de9435e2b9e2af9
Ana9R <A.Burovac@interia.pl>
2018-03-13 22:22:35 +0100

    adding implementation

--- src/main/java/task/ReverseList.java
+++ src/main/java/task/ReverseList.java
@@ -1,4 +1,25 @@
-package task;
+package task1;
+
+import java.util.ArrayList;
 
 public class ReverseList {
+    public static void main (String[] args) {
+        ArrayList<String> name = new ArrayList<String>();
+        name.add("Magdalena");
+        name.add("Patrycja");
+        name.add("Aleksandra");
+        name.add("Katarzyna");
+
+        System.out.println(name);
+
+        ArrayList <String> name2 = new ArrayList<String>();
+
+        for (int n=0; n<name.size(); n++){
+
+            name2.add(name.get(name.size()-(n+1)));
+        }
+        System.out.println(name2);
+
+    }
+
 }
